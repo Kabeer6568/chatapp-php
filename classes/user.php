2508ce45d12c9ad13b06f77d23e2185be1e121ba
Kabeer Ali Alvi <kabeeralvi6568@gmail.com>
2025-10-20 13:49:47 +0500

    Login / Signup

--- classes/user.php
+++ classes/user.php
@@ -15,4 +15,50 @@ class User{
 
     }
 
+    public function userSignup($username , $useremail , $userpass , $profile_pic){
+
+        if ($this->checkUser($useremail)) {
+            echo "<script>alert('The Email {$useremail} already exists');</script>";
+        }
+        else{
+
+        $target_dir = "uploads/";
+        $file_name = uniqid() . "_" . basename($profile_pic['name']);
+        $target_file = $target_dir . $file_name;
+
+            if (move_uploaded_file($profile_pic['tmp_name'] , $target_file)) {
+                $sql = "INSERT INTO users (username , useremail , userpass , profile_pic) VALUES 
+            (? , ? , ? , ? )";
+
+            $hashPass = password_hash($userpass , PASSWORD_BCRYPT); 
+
+            $stat = $this->pdo->prepare($sql);
+
+            $res = $stat->execute([$username , $useremail , $userpass , $target_file]);
+
+            return $res;
+        }
+        else{
+            echo "ERROR UPLOADING IMAGE";
+        }
+    }
+
+    }
+
+    public function checkUser($useremail){
+
+        $sql = "SELECT * FROM users WHERE useremail = ?";
+        $stat = $this->pdo->prepare($sql);
+
+        $stat->execute([$useremail]);
+
+        $res = $stat->rowCount() > 0;
+
+        return $res;
+
+
+    }
+
+
+
 }
--- signup.php
+++ signup.php
@@ -2,6 +2,13 @@
 <?php
 
 include "includes/header.php";
+require_once "classes/user.php";
+
+$user = new User;
+
+if (isset($_POST['submit'])) {
+    $user->userSignup($_POST['username'] , $_POST['useremail'] , $_POST['userpass'] , $_FILES['profile_pic'] );
+}
 
 ?>
 
@@ -15,22 +22,33 @@ include "includes/header.php";
             <div class="card-body">
               <h1>Register</h1>
               <p class="text-muted">Create your account</p>
-              <div class="input-group mb-3">
-                <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                <input type="text" class="form-control" placeholder="Username">
-              </div>
-              <div class="input-group mb-4">
-                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                <input type="password" class="form-control" placeholder="Password">
-              </div>
-              <div class="row">
-                <div class="col-6">
-                  <button type="button" class="btn btn-primary px-4">Register</button>
-                </div>
-                <div class="col-6 text-right">
-                  <button type="button" class="btn btn-link px-0">Forgot password?</button>
-                </div>
-              </div>
+              <form action="" method="post" enctype="multipart/form-data">
+
+                  <div class="input-group mb-3">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    <input type="text" name="username" class="form-control" placeholder="Username">
+                  </div>
+                  <div class="input-group mb-3">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    <input type="text" name="useremail" class="form-control" placeholder="Email">
+                  </div>
+                  <div class="input-group mb-4">
+                    <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                    <input type="password" name="userpass" class="form-control" placeholder="Password">
+                  </div>
+                  <div class="input-group mb-3">
+                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                    <input type="file" name="profile_pic" id="" accept="image/*">
+                  </div>
+                  <div class="row">
+                    <div class="col-6">
+                      <button type="submit" name="submit" class="btn btn-primary px-4">Register</button>
+                    </div>
+                    <div class="col-6 text-right">
+                      <button type="button" class="btn btn-link px-0">Forgot password?</button>
+                    </div>
+                  </div>
+              </form>
             </div>
           </div>
           <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
